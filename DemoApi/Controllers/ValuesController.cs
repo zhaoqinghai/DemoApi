7dace2f88ebdc725ed983c3ef19b881b1649a73b
zhaoqinghai <cayav@outlook.com>
2019-07-22 09:55:04 +0800

    demo

---
+++ DemoApi/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspectCore.Injector;
+using DemoApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [FromContainer]
+        public IBirdService BirdService { get; set; }
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            BirdService.Say();
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
---
+++ DemoApi/Services/IBirdService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoApi.Services
+{
+    public interface IBirdService
+    {
+        Task Say();
+    }
+
+    public class Duck : IBirdService
+    {
+        public Task Say()
+        {
+            Console.WriteLine("GaGaGa");
+            return Task.CompletedTask;
+        }
+    }
+}
